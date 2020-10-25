b6f6a4b4afe85badd886a5f2e8ede42323b24122
Roney Aguiar <raguiar@muxi.corp>
2020-10-25 19:31:56 -0300

    improving layout

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:follow_me/api/ig_api.dart';
 import 'package:flutter/material.dart';
 import 'package:follow_me/models/user_model.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LogInScreen extends StatelessWidget {
 
@@ -9,22 +11,65 @@ class LogInScreen extends StatelessWidget {
       IgApi api = IgApi(context);
       return Scaffold(
         appBar: AppBar(
-          title: Text("Follow Me"),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Center(child: Text("Instagram"),),
-            Center(child: Text("nome de usuário: ${UserModelSingleton().getUserName()}"),),
-            RaisedButton(
-              child: Text("log in"),
-              onPressed: (){
-                  api.authenticate();
-              },
-            ),
-          ],
-        ),
-
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(255, 129, 56, 175) , //roxo
+          title: Text("Follow Me", style: GoogleFonts.juliusSansOne(fontWeight: FontWeight.w200),),
+            elevation: 0,
+          ),
+          body: Stack(
+            alignment: Alignment.center,
+            children: [
+              Positioned(
+                child: Container(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors:[
+                          Color.fromARGB(255, 129, 56, 175), //roxo
+                          Color.fromARGB(255, 245, 133, 41), //laranja
+                        ],
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight
+                    ),
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Container(
+                          height: 500,
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white, width: 1,),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Column(
+                            children: [
+                              Text("Instagram", style: GoogleFonts.juliusSansOne(color: Colors.white)),
+                              Padding(padding: EdgeInsets.all(2),),
+                              Text( UserModelSingleton().getLogStatus() ? "nome de usuário: ${UserModelSingleton().getUserName()}" : "", style: GoogleFonts.juliusSansOne(color: Colors.white)),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 10,
+                child: Container(
+                  child: RaisedButton(
+                    color: Color.fromARGB(255, 245, 133, 41),
+                    child: Text("log in", style: GoogleFonts.juliusSansOne(color: Colors.white)),
+                    onPressed: (){
+                      api.authenticate();
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
       );
   }
 }
